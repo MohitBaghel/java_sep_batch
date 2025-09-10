00e335f903805f527ff7467b0a81b8ff30b0189b
Mohit Baghel <mohit.mynt@perennialsys.com>
2025-09-10 18:10:54 +0530

    first commit

---
+++ intro.java
@@ -0,0 +1,13 @@
+public class intro {
+    
+    public static void main(String s[]){
+        System.out.println("Hello World");
+
+    }
+}
+
+
+// public  -> access modifier -> 
+// static -> doesn't require an instance of the class to be invoked
+// void -> return type -> doesn't return anything
+// main -> entry point of the program
