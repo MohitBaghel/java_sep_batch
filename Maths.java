beacde8846d2da4ee7dcca09172c23d6ca85d399
Mohit Baghel <mohit.mynt@perennialsys.com>
2025-09-17 18:53:55 +0530

    Maths functions

---
+++ Maths.java
@@ -0,0 +1,23 @@
+public class Maths {
+
+    public static void main(String[] args) {
+        
+        // System.out.println(Math.max(10,20)); // maximum value
+        System.out.println(Math.min(12,21)); // minimum value
+        System.out.println(Math.sqrt(64)); // square root
+        System.out.println(Math.abs(57.9)); // absolute value
+        System.out.println(Math.pow(2, 4)); // random value between 0.0 to 1.0
+        System.out.println(Math.random()); // random value between 0.0 to 1.0
+        System.out.println(Math.round(Math.random() * 100)); // random value between 0.0 to 100.0
+        System.out.println(Math.round(4.3)); // round off value
+        System.out.println(Math.ceil(4.2));
+        System.out.println(Math.floor(4.9));
+
+
+        System.out.println(Math.PI); // 3.14
+        System.out.println(10 == 75);
+
+
+    }
+    
+}
