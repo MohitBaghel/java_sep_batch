f588f4551683a1f59a921217d851f52535e19c32
Mohit Baghel <mohit.mynt@perennialsys.com>
2025-09-24 18:07:36 +0530

    foor loop , while loop

---
+++ Whiledo.java
@@ -0,0 +1,39 @@
+public class Whiledo {
+
+    public static void main(String[] args) {
+        
+       
+    // while(condition){
+    //     // code
+    // }
+
+
+    // int i = 1;
+    // while(i <= 5){ // 6 <= 5
+    //     System.out.println("Counters: " + i); // Counters : 5
+    //     i++; // 6
+    // }
+    
+    
+    // System.out.println("While loop ended");
+
+
+    // int i = 0;
+    // do{
+    //         System.out.println("Counters: " + i); 
+    //         i++;
+    // } while(i<5);
+
+
+    int i = 10;
+    while(i > 0){
+        System.out.println(i); 
+        i--;
+    }
+
+    System.out.println("Happy New year 2k26!");
+
+
+}
+  
+}
---
+++ forLoop.java
@@ -0,0 +1,21 @@
+import java.beans.Statement;
+
+public class forLoop {
+
+    public static void main(String[] args) {
+        
+
+        // for(Statement1 ; Statement2 ; Statement3){
+        //     // code
+        // }
+
+
+
+        for(int i = 21; i <= 40; i++){ 
+            System.out.println("Batch 2, Roll no : " + i);
+        }
+
+        System.out.println("Batch 2 Viva is done");
+    }
+    
+}
