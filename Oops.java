71656b7141bbb4eabb5a17b0b10a23cc022656b3
Mohit Baghel <mohit.mynt@perennialsys.com>
2025-09-25 18:55:50 +0530

    methods

---
+++ Oops.java
@@ -0,0 +1,11 @@
+public class Oops { 
+
+    int x = 10;
+    public static void main(String[] args) {
+        
+    
+            Oops obj = new Oops();
+            System.out.println(obj.x);
+    }
+    
+}
