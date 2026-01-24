f156a9efad56dbed4e9c3c2b274376cc8fb82a2f
Mohit Baghel <mohit.mynt@perennialsys.com>
2026-01-24 16:50:19 +0530

    inheritanmce

---
+++ Inheritances.java
@@ -0,0 +1,27 @@
+public class Inheritances {
+
+    public static void main(String[] args) {
+        Car car1 =  new Car();
+        car1.start(); // accessing the method of superclass Vehicle
+        car1.honk();  // accessing the method of subclass Car
+    }
+
+}
+
+
+// Superclass
+ class Vehicles {
+    public void start(){
+        System.out.println("Vehicle is starting");
+    }
+}
+
+
+
+// 
+class Car extends Vehicles { // used exyend keyword to access the superclass into subclass
+        public void honk(){
+            System.out.println("Car is honking");
+        }
+    }
+
